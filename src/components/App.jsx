78b30bf30fef104b1b3e4fc66461126220fa34ee
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-03-18 18:16:22 +0200

    FinCafe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,44 @@ import { useEffect, useState } from "react";
 
 
 const App = () => {
-  const initFeedback = ({ good: 0, neutral: 0, bad: 0 });
+  const initFeedback = { good: 0, neutral: 0, bad: 0 };
+  const [feedback, setFeedback] = useState(() => {
+    const stringFeedback = localStorage.getItem("feedbackValue");
+    const parsedFeedback = JSON.parse(stringFeedback) ?? initFeedback;
+    return parsedFeedback;
+  });
  
+  const updateFeedback = (feedbackType) => {
+    setFeedback({ ...feedback, [feedbackType]: feedback[feedbackType] + 1 });
+  };
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const posFeedback = Math.round(((feedback.good + feedback.bad) / totalFeedback) * 100);
+
+  useEffect(() => {
+    localStorage.setItem("feedbackValue", JSON.stringify(feedback));
+  }, [feedback]);
+
   return (
-    <div>
-      <Options />
-      <Feedback />
-      
-    </div>
-  )
-}
-
-
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        resetFeedback={resetFeedback}
+      />
+      {totalFeedback !== 0 && (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          posFeedback={posFeedback}
+        />
+      )}
+      {totalFeedback === 0 && <Notifications/>}
+    </>
+  );
+};
 export default App
